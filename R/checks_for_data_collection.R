bbb98948f1938908650736ea6ae91c495454829a
Anthony Twesigye <twesigye10@gmail.com>
2023-05-11 13:25:54 +0300

    added correct code and name of toll and data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -5,14 +5,14 @@ library(supporteR)
 
 # read data and tool ----------------------------------------------------------
 # data
-df_tool_data <- readxl::read_excel("inputs/ETH2002_H2R_data.xlsx") |>  
+df_tool_data <- readxl::read_excel("inputs/ETH2301_MSNA_Oromia_data.xlsx") |>  
     mutate(start = as_datetime(start),
            end = as_datetime(end)) |> 
     checks_add_extra_cols(input_enumerator_id_col = "enumerator_id",
                           input_location_col = "loc_zone")
 
 # tool
-loc_tool <- "inputs/ETH2002_H2R_tool.xlsx"
+loc_tool <- "inputs/ETH2301_MSNA_Oromia_tool.xlsx"
 
 df_survey <- readxl::read_excel(loc_tool, sheet = "survey")
 df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
