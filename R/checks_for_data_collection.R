d9a15cf877c6648e6d0e11aea531872060d42775
Anthony Twesigye <twesigye10@gmail.com>
2023-05-11 13:27:34 +0300

    testing data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -21,4 +21,25 @@ df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
 
 checks_output <- list()
 
+# testing data ------------------------------------------------------------
+
+df_testing_data <- df_tool_data |> 
+    filter(i.check.start_date < as_date("2023-03-08")) |> 
+    mutate(i.check.type = "remove_survey",
+           i.check.name = "",
+           i.check.current_value = "",
+           i.check.value = "",
+           i.check.issue_id = "logic_c_testing_data",
+           i.check.issue = "testing_data",
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "", 
+           i.check.reviewed = "1",
+           i.check.adjust_log = "",
+           i.check.so_sm_choices = "") |> 
+    dplyr::select(starts_with("i.check.")) |> 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_testing_data")
 
