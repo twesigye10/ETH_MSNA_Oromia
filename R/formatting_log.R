272e410058a8eec55302576d909d7ad341516e75
Anthony Twesigye <twesigye10@gmail.com>
2023-07-08 08:56:42 +0300

    exporting the log components together

--- R/formatting_log.R
+++ R/formatting_log.R
@@ -4,6 +4,9 @@ library(tidyverse)
 library(lubridate)
 library(glue)
 library(supporteR)
+library(openxlsx)
+
+options("openxlsx.borderStyle" = "thin")
 
 # Read data and checking log 
 
@@ -101,7 +104,7 @@ df_deletion_by_enum_time <- df_cleaning_log |>
 
 # export sheets -----------------------------------------------------------
 
-
+# 
 openxlsx::write.xlsx(x = list(Summary = df_variable_summary,
                              data_extract = df_data_extract,
                              # Log_book = df_formatted_log,
@@ -110,8 +113,154 @@ openxlsx::write.xlsx(x = list(Summary = df_variable_summary,
                              changes_by_enum = df_changes_by_enum,
                              changes_by_issue = df_changes_by_enum_issue,
                              del_by_enum = df_deletion_by_enum,
-                             del_by_enum_time = df_deletion_by_enum_time), 
+                             del_by_enum_time = df_deletion_by_enum_time),
                     file = paste0("outputs/", butteR::date_file_prefix(), "_eth_msha_data_cleaning_logbook_for_formatting.xlsx"))
 
 # exporting log alone
 write_csv(df_formatted_log, file = paste0("outputs/", butteR::date_file_prefix(), "_log_book.csv", na = ""))
+
+
+# format the logbook and export -------------------------------------------
+df_variable_tracker <- tibble::tribble(
+                                  ~Variable,   ~Action,                                                                                                                                                     ~Rationale,
+                                    "i.fcs",   "Added", "computed with fs_fcs_cerealgrainroottuber, fs_fcs_beansnuts, fs_fcs_vegetableleave, fs_fcs_fruit, fs_fcs_meatfishegg, fs_fcs_dairy, fs_fcs_sugar, fs_fcs_fat",
+                                "i.fcs_cat",   "Added",                                                                                                                   "Computed based on i.fcs (thresholds 21-35)",
+                                   "i.rcsi",   "Added",                                                                                            "Computed with rCSIBorrow, rCSIMealSize, rCSIMealAdult, rCSIMealNb",
+                               "i.rcsi_cat",   "Added",                                                                                                                   "Computed based on i.rcsi (thresholds 3-18)",
+                                    "i.hhs",   "Added",                                                                         "Computed with fs_hhs_no_food_freq, fs_hhs_sleephungry_freq, fs_hhs_daynoteating_freq",
+                                "i.hhs_cat",   "Added",                                                                                                                 "Computed based on i.hhs (thresholds 1, 3, 6)",
+                                 "deviceid", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                                    "audit", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                                "audit_URL", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                            "instance_name", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                              "enum_gender", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                            "enum_phonenum", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                                "enum_name", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                                      "gps", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                            "_gps_latitude", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                           "_gps_longitude", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                            "_gps_altitude", "Removed",                                                                                                                "Blanked columns related to the survey and PII",
+                           "_gps_precision", "Removed",                                                                                                                "Blanked columns related to the survey and PII"
+                           )
+
+
+# create workbook ---------------------------------------------------------
+
+wb_log <- createWorkbook()
+
+hs1 <- createStyle(fgFill = "#E34443", textDecoration = "Bold", fontName = "Arial Narrow", fontColour = "white", fontSize = 12, wrapText = T)
+hs2 <- createStyle(fgFill = "#C4BD97", textDecoration = "Bold", fontName = "Roboto Condensed", fontColour = "white", fontSize = 11, wrapText = T)
+hs3 <- createStyle(fgFill = "#D8E4BC", textDecoration = "Bold", fontName = "Roboto Condensed", fontSize = 11, wrapText = T)
+hs4 <- createStyle(fgFill = "#D9D9D9", textDecoration = "Bold", fontName = "Roboto Condensed", fontSize = 11, wrapText = T)
+
+
+# deletion_log ------------------------------------------------------------
+
+addWorksheet(wb_log, sheetName="deletion_log")
+writeData(wb_log, sheet = "deletion_log", df_deletion_log, startRow = 1, startCol = 1)
+addStyle(wb_log, sheet = "deletion_log", hs1, rows = 1, cols = 1:6, gridExpand = FALSE)
+
+
+# data_extract ------------------------------------------------------------
+
+addWorksheet(wb_log, sheetName="data_extract")
+writeData(wb_log, sheet = "data_extract", df_data_extract, startRow = 1, startCol = 1)
+addStyle(wb_log, sheet = "data_extract", hs1, rows = 1, cols = 1:2, gridExpand = FALSE)
+
+
+# variable_tracker --------------------------------------------------------
+
+addWorksheet(wb_log, sheetName="variable_tracker")
+writeData(wb_log, sheet = "variable_tracker", df_variable_tracker, startRow = 1, startCol = 1)
+addStyle(wb_log, sheet = "variable_tracker", hs1, rows = 1, cols = 1:3, gridExpand = FALSE)
+
+
+# Enumerator - performance ------------------------------------------------
+
+addWorksheet(wb_log, sheetName="Enumerator - performance")
+
+# Dataset
+writeData(wb_log, sheet = "Enumerator - performance", "Dataset", startRow = 1, startCol = 1)
+addStyle(wb_log, sheet = "Enumerator - performance", hs2, rows = 1, cols = 1:2, gridExpand = FALSE)
+
+mergeCells(wb_log, sheet = "Enumerator - performance", rows = 3:4, cols = 1:2)
+writeData(wb_log, sheet = "Enumerator - performance", "Number of surveys collected by enumerators", startRow = 3, startCol = 1)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 1:2, gridExpand = FALSE)
+
+writeDataTable(wb = wb_log, sheet = "Enumerator - performance", 
+               x = df_surveys_by_enum , 
+               startRow = 6, startCol = 1, 
+               tableStyle = "TableStyleLight10", headerStyle = hs4)
+
+# Cleaning log 1
+writeData(wb_log, sheet = "Enumerator - performance", "Cleaning log", startRow = 1, startCol = 5)
+addStyle(wb_log, sheet = "Enumerator - performance", hs2, rows = 1, cols = 5:6, gridExpand = FALSE)
+
+mergeCells(wb_log, sheet = "Enumerator - performance", rows = 3:4, cols = 5:6)
+writeData(wb_log, sheet = "Enumerator - performance", "Number of changes by enumerators", startRow = 3, startCol = 5)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 5:6, gridExpand = FALSE)
+
+writeDataTable(wb = wb_log, sheet = "Enumerator - performance", 
+               x = df_changes_by_enum , 
+               startRow = 6, startCol = 5, 
+               tableStyle = "TableStyleLight10", headerStyle = hs4)
+
+# Cleaning log 2
+writeData(wb_log, sheet = "Enumerator - performance", "Cleaning log", startRow = 1, startCol = 9)
+addStyle(wb_log, sheet = "Enumerator - performance", hs2, rows = 1, cols = 9:11, gridExpand = FALSE)
+
+mergeCells(wb_log, sheet = "Enumerator - performance", rows = 3:4, cols = 9:11)
+writeData(wb_log, sheet = "Enumerator - performance", "Number of changes by enumerators filtered by issues", startRow = 3, startCol = 9)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 9:10, gridExpand = FALSE)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 11, gridExpand = FALSE)
+
+writeDataTable(wb = wb_log, sheet = "Enumerator - performance", 
+               x = df_changes_by_enum_issue , 
+               startRow = 6, startCol = 9, 
+               tableStyle = "TableStyleLight10", headerStyle = hs4)
+
+# Deletion log 1
+writeData(wb_log, sheet = "Enumerator - performance", "Deletion log", startRow = 1, startCol = 14)
+addStyle(wb_log, sheet = "Enumerator - performance", hs2, rows = 1, cols = 14:15, gridExpand = FALSE)
+
+mergeCells(wb_log, sheet = "Enumerator - performance", rows = 3:4, cols = 14:15)
+writeData(wb_log, sheet = "Enumerator - performance", "Number of deletions by enumerators", startRow = 3, startCol = 14)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 14:15, gridExpand = FALSE)
+
+writeDataTable(wb = wb_log, sheet = "Enumerator - performance", 
+               x = df_deletion_by_enum , 
+               startRow = 6, startCol = 14, 
+               tableStyle = "TableStyleLight10", headerStyle = hs4)
+
+# Deletion log 2
+writeData(wb_log, sheet = "Enumerator - performance", "Deletion log", startRow = 1, startCol = 18)
+addStyle(wb_log, sheet = "Enumerator - performance", hs2, rows = 1, cols = 18:19, gridExpand = FALSE)
+
+mergeCells(wb_log, sheet = "Enumerator - performance", rows = 3:4, cols = 18:19)
+writeData(wb_log, sheet = "Enumerator - performance", "Number of deletions due to time by enumerator", startRow = 3, startCol = 18)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 18:19, gridExpand = FALSE)
+
+writeDataTable(wb = wb_log, sheet = "Enumerator - performance", 
+               x = df_deletion_by_enum_time , 
+               startRow = 6, startCol = 18, 
+               tableStyle = "TableStyleLight10", headerStyle = hs4)
+
+# Deletion log 3
+writeData(wb_log, sheet = "Enumerator - performance", "Deletion log", startRow = 1, startCol = 22)
+addStyle(wb_log, sheet = "Enumerator - performance", hs2, rows = 1, cols = 22:24, gridExpand = FALSE)
+
+mergeCells(wb_log, sheet = "Enumerator - performance", rows = 3:4, cols = 22:24)
+writeData(wb_log, sheet = "Enumerator - performance", "Needs to be reviewed by Research Manager / lead AO before submission to HQ", startRow = 3, startCol = 22)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 22:23, gridExpand = FALSE)
+addStyle(wb_log, sheet = "Enumerator - performance", hs3, rows = 3:4, cols = 24, gridExpand = FALSE)
+
+writeDataTable(wb = wb_log, sheet = "Enumerator - performance", 
+               x = df_deletion_by_enum |> filter(Number > 20) |> 
+                   mutate(`issue(s) followed up in country y/n` = "Yes",
+                          `further comments` = NA_character_) |> select(-Number), 
+               startRow = 6, startCol = 22, 
+               tableStyle = "TableStyleLight10", headerStyle = hs4)
+
+
+saveWorkbook(wb_log, paste0("outputs/", butteR::date_file_prefix(),"_eth_msna_data_cleaning_logbook_oromia.xlsx"), overwrite = TRUE)
+openXL(file = paste0("outputs/", butteR::date_file_prefix(),"_eth_msna_data_cleaning_logbook_oromia.xlsx"))
