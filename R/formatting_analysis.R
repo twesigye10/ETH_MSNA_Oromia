5386d91d015d7545e11800f42dc88cfd7e1ed3e1
Anthony Twesigye <twesigye10@gmail.com>
2023-07-08 20:17:42 +0300

    formatting the analysis

--- R/formatting_analysis.R
+++ R/formatting_analysis.R
@@ -47,7 +47,13 @@ df_dap_questions <- readxl::read_excel("support_files/ETH2301_DAP_Validated.xlsx
     filter(!is.na(`Indicator / Variable`)) |> 
     janitor::clean_names() |> 
     select(in_number, indicator_group_sector, indicator_variable, questionnaire_question) |> 
-    mutate(indicator_group_sector = str_replace_all(string = indicator_group_sector, pattern = "\\/|\\?", replacement = "_"))
+    mutate(indicator_group_sector = str_replace_all(string = indicator_group_sector, pattern = "\\/|\\?", replacement = "_"),
+           indicator_group_sector = case_when(indicator_group_sector %in% c("Presentation and consent", "Respondent information","HH information") ~ "HH information",
+                                              indicator_group_sector %in% c("Cash & Markets, Livelihoods", "Cash and Markets") ~ "Cash and Markets",
+                                              indicator_group_sector %in% c("Protection", "Child Protection") ~ "Protection",
+                                              indicator_group_sector %in% c("PSNP", "Shock or vulnerability") ~ "Shock or vulnerability",
+                                              TRUE ~ indicator_group_sector)
+           )
 
 
 df_tool_dap_info <- df_tool_groups |> 
@@ -62,7 +68,8 @@ df_analysis_dap_info <- df_analysis |>
     mutate(response_lable = recode(analysis_choice_id, !!!setNames(df_choices_support$choice_label, df_choices_support$survey_choice_id)),
            choices = ifelse(is.na(response_lable), `choices/options`, response_lable),
            subset_1_val_label = recode(subset_1_val, !!!setNames(df_choices$choice_label, df_choices$choice_name)),
-           subset_1_val_label =  ifelse(is.na(subset_1_val_label), "Zonal", subset_1_val_label))
+           subset_1_val_label =  ifelse(is.na(subset_1_val_label), "Zonal", subset_1_val_label)) |> 
+    select(-c(n_unweighted, subset_1_name, subset_1_val))
 
 # split data based on groups or sectors
 output <- split(df_analysis_dap_info, df_analysis_dap_info$indicator_group_sector)
@@ -73,16 +80,21 @@ hs1 <- createStyle(fgFill = "#EE5859", halign = "CENTER", textDecoration = "Bold
 hs2 <- createStyle(fgFill = "#808080", halign = "CENTER", textDecoration = "Bold", fontColour = "white", wrapText = T)
 hs3 <- createStyle(fgFill = "#EE5859", halign = "CENTER", textDecoration = "Bold", border = "Bottom", fontColour = "white")
 
+cols_for_special_formatting <- c("Zonal", "Gasera", "Sinana", "Goba", "Harena Buluk", "Delo Mena", "Berbere", "Goro")
+
 for (i in 1:length(output)) {
     addWorksheet(wb, sheetName=names(output[i]))
     
     # add header to sheet
-    mergeCells(wb, sheet = names(output[i]), rows = 1, cols = 1:5)
+    mergeCells(wb, sheet = names(output[i]), rows = 1, cols = 1:10)
     writeData(wb, sheet = names(output[i]), names(output[i]), startCol = 1, startRow = 1, headerStyle = hs1)
-    addStyle(wb, sheet = names(output[i]), hs1, rows = 1, cols = 1:5, gridExpand = TRUE)
+    addStyle(wb, sheet = names(output[i]), hs1, rows = 1, cols = 1:10, gridExpand = TRUE)
+    
+    setColWidths(wb = wb, sheet = names(output[i]), cols = 2, widths = 60)
+    
     # get current data for the group or sector
     current_sheet_data <- output[[i]] |> 
-        
+        pivot_wider(names_from = subset_1_val_label, values_from = `Results(mean/percentage)`) |> 
         mutate(row_id = row_number())
     
     # split variables to be written in different tables with in a sheet
@@ -95,18 +107,44 @@ for (i in 1:length(output)) {
         current_variable_data <- sheet_variables_data[[j]]
         
         get_question <- current_variable_data |> select(Question) |> unique() |> pull()
+        get_qn_type <- current_variable_data |> select(select_type) |> unique() |> pull()
+        
+        if(get_qn_type %in% c("select_one", "select one", "select_multiple", "select multiple")){
+            class(current_variable_data$Zonal) <- "percentage"
+            class(current_variable_data$Gasera) <- "percentage"
+            class(current_variable_data$Sinana) <- "percentage"
+            class(current_variable_data$Goba) <- "percentage"
+            class(current_variable_data$`Harena Buluk`) <- "percentage"
+            class(current_variable_data$`Delo Mena`) <- "percentage"
+            class(current_variable_data$Berbere) <- "percentage"
+            class(current_variable_data$Goro) <- "percentage"
+        }else{
+            class(current_variable_data$Zonal) <- "numeric"
+            class(current_variable_data$Gasera) <- "numeric"
+            class(current_variable_data$Sinana) <- "numeric"
+            class(current_variable_data$Goba) <- "numeric"
+            class(current_variable_data$`Harena Buluk`) <- "numeric"
+            class(current_variable_data$`Delo Mena`) <- "numeric"
+            class(current_variable_data$Berbere) <- "numeric"
+            class(current_variable_data$Goro) <- "numeric"
+        }
         
         variable_data_length <- previous_max_row + 3
-        # current_variable_data
+    
         print(variable_data_length)
         
         # add header for variable
-        mergeCells(wb, sheet = names(output[i]), rows = previous_max_row + 2, cols = 1:5)
+        mergeCells(wb, sheet = names(output[i]), rows = previous_max_row + 2, cols = 1:10)
         writeData(wb, sheet = names(output[i]), get_question, startCol = 1, startRow = previous_max_row + 2)
-        addStyle(wb, sheet = names(output[i]), hs2, rows = previous_max_row + 2, cols = 1:5, gridExpand = TRUE)
+        addStyle(wb, sheet = names(output[i]), hs2, rows = previous_max_row + 2, cols = 1:10, gridExpand = TRUE)
         writeDataTable(wb = wb, 
                        sheet = names(output[i]), 
-                       x = current_variable_data |> select(variable, choices, `Results(mean/percentage)`), 
+                       x = current_variable_data |> 
+                           select(-c(Question, `choices/options`, 
+                                     population, analysis_choice_id, 
+                                     indicator_group_sector,response_lable,
+                                     row_id, variable, select_type, indicator_variable)
+                                                           ), 
                        startRow = variable_data_length, 
                        startCol = 1, 
                        tableStyle = "TableStyleLight9", 
@@ -116,6 +154,11 @@ for (i in 1:length(output)) {
     }
     
 }
+
+# worksheets order
+worksheetOrder(wb) <- c(7, 2, 8, 4, 3, 10, 13, 11, 6, 5, 1, 9, 12)
+
+activeSheet(wb) <- 7
 
 saveWorkbook(wb, paste0("outputs/", butteR::date_file_prefix(),"_formatted_analysis_eth_msna_oromia.xlsx"), overwrite = TRUE)
 openXL(file = paste0("outputs/", butteR::date_file_prefix(),"_formatted_analysis_eth_msna_oromia.xlsx"))
